f758fd5700bea1086689c9185686dcaa34876501
rabeezarre <rabina.abdrakhmanova@nu.edu.kz>
2022-05-21 03:13:03 +0600

    added styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="shadow-md py-10 bg-blue-700 mb-8">
+    <h1 class="text-3xl font-bold underline inline text-white ml-8">
+      Weather forecast
+    </h1>
+    <h2 class="text-3xl font-normal inline ml-4 text-white">{{currentDate()}}</h2>
+  </div>
+
+  <CityList
+    v-bind:cities="cities"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CityList from '@/components/CityList'
 export default {
   name: 'App',
+  data() {
+    return {
+      cities: ['Karagandy', 'Batman', 'Okay', 'Dinosaur', 'Bukhalovo']
+    }
+  },
   components: {
-    HelloWorld
+    CityList
+  },
+  methods: {
+    currentDate() {
+      const current = new Date();
+      const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
+      return date;
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<!--<style>-->
+<!--#app {-->
+<!--  font-family: Avenir, Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  text-align: center;-->
+<!--  color: #2c3e50;-->
+<!--  margin-top: 60px;-->
+<!--}-->
+<!--</style>-->
